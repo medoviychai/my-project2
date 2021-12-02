07e91face9d13d0714b15cc7fa5be7ab84ef04e3
maria <xelam4444@mail.ru>
2021-12-02 21:12:42 +0300

    finish code

--- task_tracker.js
+++ task_tracker.js
@@ -1,4 +1,3 @@
-const task = document.querySelector('.task-wrapper');
 const buttonAddTask = document.querySelector('.add');
 const tasks = document.querySelector('.task');
 
@@ -17,12 +16,10 @@ buttonAddTask.addEventListener('click', (event) => {
 
 //Удаление задачи
 document.addEventListener("click", event => {
-   //(элемент, на котором событие сработало)
    // console.log(event.target);
    const removeButton = event.target.closest('.remove-task');
    
-   if (removeButton) {//если мы нажали на элемент с классом remove-task
-     //то можно "выпасть до родительской формы:"
+   if (removeButton) {
      let formTaskWrap = removeButton.closest('form.task-wrapper');
  
      if (formTaskWrap) {
@@ -32,82 +29,64 @@ document.addEventListener("click", event => {
 });
 
 
-
-
-
-
-
-
-
-
-
 //Сортировка задач
 const sortTasks = document.querySelector('.sort');
 
-// sortTasks.addEventListener('click', () => {
-//    sortTasks.className = 'sort-reverse';
+let abc = true;
 
-
-   // let sort = Array.from(entryField).slice(1).sort((a, b) => a.value > b.value ? 1 : -1); 
    sortTasks.addEventListener('click', () => {
-      sortTasks.className = 'sort-reverse';
+      if (abc) {
+         sortTasks.className = 'sort-reverse';
 
-      const entryField = document.querySelectorAll('.entry-field');
+         const entryField = document.querySelectorAll('.entry-field');
 
-      let sortT = [];
-      entryField.forEach(item => {
-         sortT.push(item.value);
-      })
-      let sortTask = sortT.sort();
-      console.log(sortTask);
-
-
-      // let sort = Array.from(entryField.values).slice(1)
-      // console.log(sort);
-     
-      sortTask.forEach(item => {
-         // entryField.value = item;
-         // console.log(entryField.value);
-         // console.log(item);
-         const t = document.createElement('input');
-         t.className = 'entry-field';
-         t.value = item;
-
-         const tasksAll = document.querySelectorAll('.task-wrapper');
-         tasksAll.forEach(item => {
-
-            item.append(t);
-            console.log(item);
+         let sortT = [];
+         entryField.forEach(item => {
+            sortT.push(item.value);
          })
+         let sortTask = sortT.sort();
+         // console.log(sortTask);
 
-         // tasksAll.forEach(item => {
-         //    entryField.forEach(elem => {
-         //       item.replaceChild(sortTasks, elem);
+         let formTaskWrap = document.querySelectorAll('form.task-wrapper');
+         formTaskWrap.forEach(elem => {
+            elem.remove();
+         });
 
-         //    })
-
-         // entryField.forEach(elem => {
-         //    elem.replaceWith(sortTask)
-         // })
+         sortTask.forEach(item => {
+            tasks.insertAdjacentHTML("beforeend", 
+               `<form class='task-wrapper'> 
+                  <input class='entry-field' value='${item}'>
+                  <button type="button" name ='remove-task' class='remove-task'></button>
+               </form>`);
          })
-      })
-
-      //  sortTask.reverse();
-
+         abc = false;
+      }
 
 
-   
-   // let sort = [document.querySelectorAll('.entry-field')]
-   // console.log(sort.textContent);
+      else {
+         const entryField = document.querySelectorAll('.entry-field');
 
+         let sortT = [];
+         entryField.forEach(item => {
+            sortT.push(item.value);
+         });
+         let sortTask = sortT.sort().reverse();
+         // console.log(sortTask);
 
-   // let sortTask = [...document.querySelector('.entry-field').value].sort();
- 
-   // sortTasks.addEventListener('click', () => {
-   //    sortTasks.className = 'sort-reverse';
-   //    sortTask.forEach(elem => task.append(elem));
-   //    sortTask.reverse();
-   // });
-// })
+         let formTaskWrap = document.querySelectorAll('form.task-wrapper');
+         formTaskWrap.forEach(elem => {
+            elem.remove();
+         });
 
-const entryField = document.querySelectorAll('.entry-field');
+         sortTask.forEach(item => {
+            tasks.insertAdjacentHTML("beforeend", 
+               `<form class='task-wrapper'> 
+                  <input class='entry-field' value='${item}'>
+                  <button type="button" name ='remove-task' class='remove-task'></button>
+               </form>`);
+         });
+         sortTasks.className = 'sort';
+
+         abc = true;
+      }
+   });
